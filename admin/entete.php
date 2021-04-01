f552a9a065bd4c519c078b7845aff8ed9b812693
Corentin Maille <maillecorentin@pm.me>
2021-04-01 16:42:24 +0200

    modification des navbar

--- admin/entete.php
+++ admin/entete.php
@@ -15,59 +15,62 @@ if(!empty($_SESSION["habilitation"]) && $_SESSION["habilitation"] == "admin")
 
     </head>
     <body style="overflow-x: hidden;">
-    <nav class="navbar navbar-dark navbar-expand-md bg-dark">
-        <a class="navbar-brand" href="../index.php">
-            <img src="../images/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
-            Projet gestion projets
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <nav class="navbar navbar-dark navbar-expand-md bg-dark w-100" style="height: 8vh; position:fixed">
+            <a class="navbar-brand" href="../index.php">
+                <img src="../images/logo.png" width="30vh" height="30vh" class="d-inline-block align-top" alt="">
+                Projet gestion projets
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <div class="navbar-nav w-100" style="padding-right: 5vh;">
-                
-                <div style="margin-left:auto">
-                    <a class="btn btn-danger" href="../traitements/deconnexion.php">Déconnexion</a>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <div class="navbar-nav w-100" style="padding-right: 5vh;">
+                    
+                    <div style="margin-left:auto">
+                        <a class="btn btn-danger" href="../traitements/deconnexion.php">Déconnexion</a>
+                    </div>
+
                 </div>
+            </div>
+
+        </nav>
+        <div class="row" style="height: 8vh;">
+        </div>
+    <div class="row">
+            <div class="col-2">
+            </div>
+            <div class="text-center pt-2 bg-light col-2" style="height: 95vh; border: 1px solid rgba(0, 0, 0, 0.125);position: fixed">
+                <nav>
+                    <ul class="list-unstyled">
+                        <li><a href="index.php" class="btn btn-outline-secondary w-75">Accueil</a></li>
+                        <li>
+                            <button class="btn btn-outline-primary w-75 mt-4" data-bs-toggle="collapse" data-bs-target="#equipesCollapse" aria-expanded="false">Utilisateurs</button>
+
+                            <div id="equipesCollapse" class="collapse">
+                                <ul class="list-unstyled">
+                                    <li><a class="btn btn-outline-primary w-75 mt-2" href="equipesAdmin.php">Vue d'ensemble</a></li>
+                                    <li><a class="btn btn-outline-secondary w-75 mt-2" href="inscriptionUtilisateur.php">Inscriptions</a></li>
+                                    <li><a class="btn btn-outline-secondary w-75 mt-2" href="listeMembres.php">Liste des membres</a></li>
+                                    <li><a class="btn btn-outline-secondary w-75 mt-2" href="gererEntreprise.php">Postes et Équipes</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="mt-2">
+                            <button class="btn btn-outline-primary w-75 mt-4" data-bs-toggle="collapse" data-bs-target="#organisationCollapse" aria-expanded="false">Organisation</button>
+
+                            <div id="organisationCollapse" class="collapse">
+                                <ul class="list-unstyled">
+                                    <li><a class="btn btn-outline-primary w-75 mt-2" href="gestionOrganisation.php">Vue d'ensemble</a></li>
+                                    <li><a class="btn btn-outline-secondary w-75 mt-2" href="#">Option 1</a></li>
+                                </ul>
+                            </div>
+                        
+                        </li>
+                    </ul>
+                </nav>
 
             </div>
-        </div>
-
-    </nav>
-    <div class="row">
-        <div class="col-2 text-center pt-2 bg-light" style="height: 95vh; border: 1px solid rgba(0, 0, 0, 0.125)">
-            <nav>
-                <ul class="list-unstyled">
-                    <li><a href="index.php" class="btn btn-outline-secondary w-75">Accueil</a></li>
-                    <li>
-                        <button class="btn btn-outline-primary w-75 mt-4" data-bs-toggle="collapse" data-bs-target="#equipesCollapse" aria-expanded="false">Utilisateurs</button>
-
-                        <div id="equipesCollapse" class="collapse">
-                            <ul class="list-unstyled">
-                                <li><a class="btn btn-outline-primary w-75 mt-2" href="equipesAdmin.php">Vue d'ensemble</a></li>
-                                <li><a class="btn btn-outline-secondary w-75 mt-2" href="inscriptionUtilisateur.php">Inscriptions</a></li>
-                                <li><a class="btn btn-outline-secondary w-75 mt-2" href="listeMembres.php">Liste des membres</a></li>
-                                <li><a class="btn btn-outline-secondary w-75 mt-2" href="gererEntreprise.php">Postes et Équipes</a></li>
-                            </ul>
-                        </div>
-                    </li>
-                    <li class="mt-2">
-                        <button class="btn btn-outline-primary w-75 mt-4" data-bs-toggle="collapse" data-bs-target="#organisationCollapse" aria-expanded="false">Organisation</button>
-
-                        <div id="organisationCollapse" class="collapse">
-                            <ul class="list-unstyled">
-                                <li><a class="btn btn-outline-primary w-75 mt-2" href="gestionOrganisation.php">Vue d'ensemble</a></li>
-                                <li><a class="btn btn-outline-secondary w-75 mt-2" href="#">Option 1</a></li>
-                            </ul>
-                        </div>
-                    
-                    </li>
-                </ul>
-            </nav>
-
-        </div>
-
     <?php
 } else {
     header("location:../index.php");

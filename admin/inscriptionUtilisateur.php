e18c0941b0fc37322f24b54a49b13dbc1e797f98
Barbier Nathan <n.barbier001@gmail.com>
2021-04-02 14:29:29 +0200

    je bug

--- admin/inscriptionUtilisateur.php
+++ admin/inscriptionUtilisateur.php
@@ -1,44 +1,43 @@
 <?php require_once "entete.php";
 ?>
+<<<<<<< HEAD
 <div class="col-9 container mt-5">
 
 <h1>Inscription d'un utilisateur</h1>
+=======
+>>>>>>> 8fdd04aa91654970e469b2aa8e2258f03bb110a9
 
-<form method="post" action="../traitements/inscriptionUtilisateur.php">
-        
-    <div class="form-group">
-        <label for="email">Adresse mail</label>
-        <input class="form-control" type="email" name="email" id="email" value="<?= isset($email) ? $email : ""?>"  required>
-    </div>
+<div class="col-9 mt-4">
+    <div class="container">
+        <h2>Inscription des membres</h2>
+        <form method="post" action="../traitements/inscriptionUtilisateur.php">
+                
+            <div class="form-floating mt-3 mb-3">
+                <input class="form-control" type="email" name="email" id="email" placeholder="adresse email" value="<?= isset($email) ? $email : ""?>"  required>
+                <label for="email">Adresse email</label>
+            </div>
 
-    <div class="form-group">
-        <label for="nom">Nom</label>
-        <input class="form-control" type="nom" name="nom" id="nom" value="<?= isset($nom) ? $nom : ""?>"  required>
-    </div>
+            <div class="form-floating mb-3">
+                <input class="form-control" type="text" name="nom" id="nom" placeholder="Nom" value="<?= isset($nom) ? $nom : ""?>"  required>
+                <label for="nom">Nom</label>
+            </div>
 
-    <div class="form-group">
-        <label for="prenom">Prénom</label>
-        <input class="form-control" type="prenom" name="prenom" id="prenom" value="<?= isset($prenom) ? $prenom : ""?>"  required>
-    </div>
-    
-    <div class="form-group">
-        <label for="dateNaiss">Date de naissance</label>
-        <input class="form-control" type="date" name="dateNaiss" id="dateNaiss" placeholder="AAAA-MM-JJ" value="<?= isset($dateNaiss) ? $dateNaiss : ""?>"  required>
-    </div>
+            <div class="form-floating mb-3">
+                <input class="form-control" type="text" name="prenom" id="prenom" placeholder="Prénom" value="<?= isset($prenom) ? $prenom : ""?>"  required>
+                <label for="prenom">Prénom</label>
+            </div>
+            
+            <div class="form-floating mb-3">
+                <input class="form-control" type="date" name="dateNaiss" id="dateNaiss" placeholder="AAAA-MM-JJ" value="<?= isset($dateNaiss) ? $dateNaiss : ""?>"  required>
+                <label for="dateNaiss">Date de naissance</label>
+            </div>
 
-    <div class="row text-center ">
-        <div class="col" style="width:40%;">
-            <label for="idPoste">Sélectionnez un poste</label>
-            <select class="form-control col text-center" name="idPoste" id="idPoste">
-                <?php
-                    // récupérer postes de l'organisation
-                    $nomPostes = recupererPostes($_SESSION["idOrganisation"]);
-                    
-                    foreach($nomPostes as $nomPoste)
-                    {
-                        ?>
-                        <option value="<?= $nomPoste["idPoste"] ?>"><?= $nomPoste["nomPoste"] ?></option>
+            <div class="row text-center ">
+                <div class="col" style="width:40%;">
+                    <label for="idPoste">Sélectionnez un poste</label>
+                    <select class="form-control col text-center" name="idPoste" id="idPoste">
                         <?php
+<<<<<<< HEAD
                     }
                     
                     ?>
@@ -55,20 +54,47 @@
                     {
                         ?>
                         <option value="<?= $nomEquipe["idEquipe"] ?>"><?= $nomEquipe["nomEquipe"] ?></option>
+=======
+                            // récupérer postes de l'organisation
+                            $nomPostes = recupererPostes($_SESSION["idOrganisation"]);
+                            
+                            foreach($nomPostes as $nomPoste)
+                            {
+                                ?>
+                                <option value="<?= $nomPoste["idPoste"] ?>"><?= $nomPoste["nomPoste"] ?></option>
+                                <?php
+                            }
+                            
+                            ?>
+                    </select>
+                </div>
+                <div class="col" style="width:40%">
+                    <label for="idEquipe">Sélectionnez une équipe</label>
+                    <select class="form-control col text-center" name="idEquipe" id="idEquipe">
+>>>>>>> 8fdd04aa91654970e469b2aa8e2258f03bb110a9
                         <?php
-                    }
-                        ?>
-            </select>
-        </div>
-    
-    
+                            // récupérer équipes de l'organisation
+                            $nomEquipes = recupererEquipes($_SESSION['idOrganisation']);
+                            
+                            foreach($nomEquipes as $nomEquipe)
+                            {
+                                ?>
+                                <option value="<?= $nomEquipe["idEquipe"] ?>"><?= $nomEquipe["nomEquipe"] ?></option>
+                                <?php
+                            }
+                                ?>
+                    </select>
+                </div>
+            
+            
+            </div>
+            <div class="text-center mt-5">
+                <button style="min-width: 15%" type="submit" class="btn btn-primary" name="envoi" value="1">Inscrire</button>
+            </div>
+
+
+        </form>
     </div>
-    <div class="text-center mt-5">
-        <button style="min-width: 15%" type="submit" class="btn btn-primary" name="envoi" value="1">Inscrire</button>
-    </div>
-
-
-</form>
-
+</div>
 <?php
 require_once 'pied.php' ?>

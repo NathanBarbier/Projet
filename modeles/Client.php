68eec35e47d637852fa4f4e3e765dd0c754bdec7
NathanBarbier <81628062+NathanBarbier@users.noreply.github.com>
2021-04-15 18:22:31 +0200

    Update Client.php

--- modeles/Client.php
+++ modeles/Client.php
@@ -1,4 +1,39 @@
 <?php
+
+Class Client extends Modele 
+{
+    private $idClient;
+    private $nom;
+
+    public function __construct($idClient = null)
+    {
+        if($idClient !== null)
+        {
+            $requete = $this->getBdd()->prepare("SELECT * FROM clients WHERE idClient = ?");
+            $requete->execute([$idClient]);
+            $client = $requete->fetch(PDO::FETCH_ASSOC);
+            $this->idClient = $idClient;
+            $this->nom = $client["nom"];
+        }
+    }
+
+    public function getIdCLient()
+    {
+        return $this->idClient;
+    }
+
+    public function getNomCLient()
+    {
+        return $this->nom;
+    }
+
+    public function setNomCLient($nom)
+    {
+        $this->nom = $nom;
+    }
+
+}
+
 function recupererClientOrganisation($idOrganisation)
 {
     $requete = getBdd()->prepare("SELECT clients.nom FROM clients LEFT JOIN projets USING(idClient) LEFT JOIN travaille_sur USING(idProjet) LEFT JOIN equipes USING(idEquipe) WHERE equipes.idOrganisation = ?");

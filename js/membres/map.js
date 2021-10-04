cad60b80e6b646023faa58fa9f21a807f4490e15
CorentinMaille <maillecorentin@pm.me>
2021-10-04 19:34:43 +0200

    map.php / ajax / js

--- js/membres/map.js
+++ js/membres/map.js
@@ -1,50 +1,126 @@
-$(".add-task-btn").click(function() {
-    $(this).parent().next().prepend("<div class='task-bubble mt-2 mb-1 mx-2'><textarea class='task-bubble-input'></textarea></div><a class='ms-2 btn btn-outline-dark task-check collapse'>Check</a><a class='ms-2 btn btn-outline-danger task-delete collapse'>Delete</a>");
+init();
+newTaskInit();
 
-    
-    $(".task-bubble").hover(function() {
-        $(this).css({"background-color": "#eeeff0", "cursor": "pointer"});
-        $(this).children().css({"background-color": "#eeeff0", "cursor": "pointer"});
-    }, function() {
-        $(this).css({"background-color": "white", "cursor": "default"});
-        $(this).children().css({"background-color": "white", "cursor": "default"});
-    });
-
-    $(".task-bubble-input").focus(function() {
-        $(this).parent().next().addClass('show');
-        $(this).parent().next().next().addClass('show');
-    });
-
-    $(".task-bubble").click(function() {
-        $(this).children().focus();
-    });
-
-    $(".task-check").click(function() {
-        $(this).removeClass("show");
-        $(this).next().removeClass("show");
-    });
-    $(".task-delete").click(function() {
-        // remove task html
-        $(this).prev().remove();
-        $(this).prev().remove();
-        $(this).remove();   
-    });
-
-
-});
-
+// NEW COLUMN
 $("#add-column-btn").click(function() {
     $("#add-column-form").toggleClass('show');
 });
 
 $("#add-column-form").find('button').click(function() {
+    
+    columnName = $("#columnName-input").val();
+    
     // insert in bdd
+    $.ajax({
+        url: CONTROLLERS_URL+"membres/map.php?action=addColumn&columnName="+columnName+"&projectId="+projectId,
+        success: function(result) {
+            console.log(result);
+        }
+    });    
 
     // create html
-    columnName = $(this).prev().val();
     $(this).prev().val("");
     $(this).toggleClass('show');
 
     $("#add-column-btn").parent().prev().after("<div class='project-column'><div class='column-title text-center pt-2'><b>"+columnName+"</b><button class='btn btn-outline-dark add-task-btn me-3'>New</button></div><div class='column-content'></div></div>")
+
+    newTaskInit();
 });
 
+
+
+function newTaskInit()
+{
+    $(".add-task-btn").click(function() {
+        $(this).parents(".column-title").next().prepend("<div class='task-bubble mt-2 pt-3 mb-1 mx-2'><textarea class='task-bubble-input text-center'></textarea></div><a class='ms-2 btn btn-outline-dark task-check collapse'>Check</a><a class='ms-2 btn btn-outline-danger task-delete collapse'>Delete</a>");
+        
+        // INSERT NEW TASK IN BDD
+        columnId = $(this).parents(".column-title").prev().val();
+
+        console.log(columnId)
+
+        $.ajax({
+            url: CONTROLLERS_URL+"membres/map.php?action=addTask&columnId="+columnId+"&projectId="+projectId,
+            success: function(result) {
+                console.log(result);
+            }
+        });
+
+        init();
+        
+    });
+}
+
+function init()
+{
+    $(".delete-col-btn").click(function() {
+        // GIT COLUMN ID
+        columnId = $(this).parents(".column-title").prev().val();
+        // DELETE HTML COLUMN
+        $(this).parents(".project-column").remove();
+
+        console.log(columnId, projectId);
+        
+        // DELETE COLUMN IN BDD
+        $.ajax({
+            url: CONTROLLERS_URL+"membres/map.php?action=deleteColumn&columnId="+columnId+"&projectId="+projectId,
+            success: function(result) {
+                console.log(result);
+            }
+        });
+
+    });
+
+    $(".task-bubble").hover(function() {
+        $(this).css({"background-color": "#eeeff0", "cursor": "pointer"});
+        $(this).children().css({"background-color": "#eeeff0", "cursor": "pointer"});
+    }, function() {
+        $(this).css({"background-color": "white", "cursor": "default"});
+    $(this).children().css({"background-color": "white", "cursor": "default"});
+    });
+    
+    $(".task-bubble-input").focus(function() {
+        $(this).parent().nextAll(".task-check").first().addClass('show');
+        $(this).parent().nextAll(".task-delete").first().addClass('show');
+    });
+    
+    $(".task-bubble").click(function() {
+        $(this).children().focus();
+    });
+    
+    $(".task-check").click(function() {
+        $(this).removeClass("show");
+        $(this).nextAll(".task-delete").first().removeClass("show");
+
+        taskName = $(this).prevAll(".task-bubble").first().find(".task-bubble-input").val();
+        taskId = $(this).prevAll(".taskId-input").first().val();
+
+        // INSERT INTO BDD
+        $.ajax({
+            url: CONTROLLERS_URL+"membres/map.php?action=updateTask&taskId="+taskId+"&taskName="+taskName+"&projectId="+projectId,
+            success: function(result) {
+                console.log(result);
+            }
+        });
+
+    });
+    $(".task-delete").click(function() {
+
+        taskId = $(this).prevAll(".taskId-input").first().val();
+        // DELETE FROM BDD
+        console.log(taskId, projectId);
+
+        $.ajax({
+            url: CONTROLLERS_URL+"membres/map.php?action=deleteTask&taskId="+taskId+"&projectId="+projectId,
+            success: function(result) {
+                console.log(result);
+            }
+        });
+
+        // remove task html
+        $(this).prevAll(".task-check").first().remove();
+        $(this).prevAll(".task-bubble").first().remove();
+        $(this).prevAll(".taskId-input").first().remove();
+        $(this).remove();
+    });   
+}
--- views/membres/map.php
+++ views/membres/map.php
@@ -26,6 +26,9 @@ else if ($errors)
 <?php
 } ?>
 
+<div id="#success" class="alert alert-success mx-3 collapse"></div>
+<div id="#error" class="alert alert-danger mx-3 collapse"></div>
+
 
 <div class="row ms-3 me-4 overflow-x" style="height: 90%;">
     <?php
@@ -33,12 +36,28 @@ else if ($errors)
     {
     ?>
         <div class="project-column">
+            <input class="columnId-input" type="hidden" value="<?= $column->getRowid() ?>">
             <div class="column-title text-center pt-2">
-                <b><?= $column->getName() ?></b>
-                <button class="btn btn-outline-dark add-task-btn me-3">New</button>
+                <ul>
+                    <li class="me-2"><b><?= $column->getName() ?></b><button class="btn btn-outline-dark add-task-btn">New</button></li>
+                    <li class="mt-2 me-2"><button class="btn btn-outline-danger delete-col-btn">Delete</button></li>
+                </ul>
             </div>
             <div class="column-content">
-                
+                <?php 
+                foreach($column->getTasks() as $task)
+                {   
+                    // var_dump($task);
+                    ?>
+                    <input class="taskId-input" type="hidden" value="<?= $task->getRowid() ?>">
+                    <div class='task-bubble mt-2 pt-3 mb-1 mx-2'>
+                        <textarea class='task-bubble-input text-center'><?= $task->getName() ?></textarea>
+                    </div>
+                    <a class='ms-2 btn btn-outline-dark task-check collapse'>Check</a>
+                    <a class='ms-2 btn btn-outline-danger task-delete collapse'>Delete</a>
+                    <?php
+                }
+                ?>
             </div>
         </div>
             
@@ -48,14 +67,16 @@ else if ($errors)
         <button id="add-column-btn" class="btn btn-outline-dark" style="width:max-content; height:min-content; line-height:80%">Add Column</button>
        
         <div id="add-column-form" class="sticker text-center mt-2 py-2 collapse" style="height: max-content;">
-            <input class="form-control w-75 mx-auto text-center" type="text">
+            <input id="columnName-input" class="form-control w-75 mx-auto text-center" type="text">
             <button class="btn btn-outline-primary w-75 mt-2">Create</button>
         </div>
     </div>
 </div>
 
 <script>
-    var IMG_URL = <?php echo json_encode(IMG_URL); ?>;
+    const IMG_URL = <?php echo json_encode(IMG_URL); ?>;
+    const CONTROLLERS_URL = <?php echo json_encode(CONTROLLERS_URL); ?>;
+    var projectId = <?php echo json_encode($CurrentProject->getId()); ?>;
 </script>
 
 <script type="text/Javascript" src="<?= JS_URL ?>membres/map.js"></script>

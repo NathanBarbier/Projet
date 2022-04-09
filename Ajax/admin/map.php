7623a33a7c39629fb841216cac03c6f533838e6f
Corentin Maille <maillecorentin@protonmail.com>
2022-04-09 18:34:51 +0200

    - (Fix) A log creation causing server to crash in ajax/admin/map.php

--- Ajax/admin/map.php
+++ Ajax/admin/map.php
@@ -79,6 +79,8 @@ if( isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) && ( $_SERVER['HTTP_X_REQUESTED_W
                             if($taskId && $Team->checkTask($taskId))
                             {
                                 try {
+                                    $TaskComment = new TaskComment();
+
                                     // prepare comment
                                     $TaskComment->setFk_task($taskId);
                                     $TaskComment->setFk_user($idUser);
@@ -95,7 +97,7 @@ if( isset( $_SERVER['HTTP_X_REQUESTED_WITH'] ) && ( $_SERVER['HTTP_X_REQUESTED_W
     
                                     echo json_encode($TaskComment);
     
-                                	LogHistory::create($idUser, 'create', 'task_comment', $commentId, "task", $TaskComment->getFk_task(), $idOrganization, "INFO", null, $ip, $page);
+                                	LogHistory::create($idUser, 'create', 'task_comment', $commentId, "task", $TaskComment['rowid'], $idOrganization, "INFO", null, $ip, $page);
     
                                 } catch (\Throwable $th) {
                                     echo json_encode(false);
